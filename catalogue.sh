6b924c6c7782e67771f85b064a058b381a4ebb7c
keerthi <reddychitti15@gmail,com>
2025-09-25 16:32:32 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -75,7 +75,9 @@ VALIDATE $? "Copy mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install Mongodb client"
 
-mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+mongo --host $MONGODB_HOST < /app/schema/catalogue.js &>>$LOG_FILE
+
+#mongosh --host $MONGODB_HOST < /app/db/master-data.js &>>$LOG_FILE
 VALIDATE $? "Load catalogue products"
 
 systemctl restart catalogue
