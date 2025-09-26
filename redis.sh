e83eb4321d9244b8e1a967adcc4e8b774c789815
keerthi <reddychitti15@gmail,com>
2025-09-26 15:35:01 +0530

    cart

--- redis.sh
+++ redis.sh
@@ -57,11 +57,10 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disabling Default Redis"
-dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Redis 7"
-dnf install redis -y  &>>$LOG_FILE
+amazon-linux-extras enable redis6 &>>$LOG_FILE
+VALIDATE $? "Enabling Redis 6 via Amazon Linux Extras"
+
+yum install -y redis &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
