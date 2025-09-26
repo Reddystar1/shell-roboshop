22ffb7dd8ede99d67e0d84b8c8e28b4614c0df06
keerthi <reddychitti15@gmail,com>
2025-09-26 19:56:52 +0530

    cart

--- mongodb.sh
+++ mongodb.sh
@@ -42,7 +42,14 @@ VALIDATE $? "Start MongoDB"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections to MongoDB"
 
+node server.js
+VALIDATE $? ""
+
 systemctl restart mongod
 VALIDATE $? "Restarted MongoDB"
+
+systemctl npm install
+systemctl npm start
+
 
 sudo systemctl status mongod
