93a43344ca9745af66178bbd0a7fd016d7d44822
keerthi <reddychitti15@gmail,com>
2025-09-26 07:50:51 +0530

    roboshop

--- 1-roboshop.sh
+++ 1-roboshop.sh
@@ -21,14 +21,14 @@ do
     echo "$instance: $IP"
 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id Z10225341OKKJ5ODTVYKC \
+    --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
         "Comment": "Updating record set"
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
+            "Name"              : "'mongodb.daws86.space'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
