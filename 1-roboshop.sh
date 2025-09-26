bd037ec1faf34a0116d9bcb122bb11747020b9da
keerthi <reddychitti15@gmail,com>
2025-09-26 08:08:09 +0530

    roboshop

--- 1-roboshop.sh
+++ 1-roboshop.sh
@@ -29,26 +29,6 @@ do
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
             "Name"              : "'mongodb.daws86.space'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
-        }]
-    }
-    '
-##catalogue
- echo "$instance: $IP"
-
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Updating record set"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
             "Name"              : "'catalogue.daws86.space'"
             ,"Type"             : "A"
             ,"TTL"              : 1
@@ -59,4 +39,5 @@ do
         }]
     }
     '
+
 done
