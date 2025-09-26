06f8cfcc9e695727e640f034ca113d2af3f83b40
keerthi <reddychitti15@gmail,com>
2025-09-26 18:52:37 +0530

    cart

--- frontend.sh
+++ frontend.sh
@@ -49,7 +49,7 @@ rm -rf /etc/nginx/nginx.conf
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
 
-systemctl status nginx.service
+sudo systemctl status nginx.service
 
 systemctl restart nginx 
 VALIDATE $? "Restarting Nginx"
