75b52dbb6bf2cfe6fa43ace5f95fd19678e88a8d
keerthi <reddychitti15@gmail,com>
2025-09-26 16:22:58 +0530

    cart

--- redis.sh
+++ redis.sh
@@ -70,6 +70,11 @@ VALIDATE $? "Allowing Remote connections to Redis"
 systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling Redis"
 
+sudo mkdir -p /var/lib/redis /var/run/redis
+sudo chown -R redis:redis /var/lib/redis /var/run/redis
+sudo systemctl restart redis
+
+
 sudo systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting Redis"
 
